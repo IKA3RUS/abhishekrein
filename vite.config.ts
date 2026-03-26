c1b615c4b8b8eb52dc42549ccac81e34c4e87ef9
IKA3RUS <w@abhishekrein.xyz>
2026-03-26 21:22:05 +0530

    build: upgrade vite to v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
@@ -8,15 +7,20 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
+import svgr from "vite-plugin-svgr";
+
 const config = defineConfig({
   plugins: [
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    svgr(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
 
 export default config;
