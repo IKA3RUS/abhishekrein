904b52c3c164dadd05dedc3d0d17689d9b8712f5
IKA3RUS <w@abhishekrein.xyz>
2026-03-28 03:44:23 +0530

    build: remove nitro

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 
 import svgr from "vite-plugin-svgr";
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    svgr(),
-  ],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), svgr()],
+  server: {
+    host: true,
+  },
   resolve: {
     tsconfigPaths: true,
   },
