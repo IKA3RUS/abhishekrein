02cdc905ce271a531f3a0174e05868b937aaf64f
IKA3RUS <w@abhishekrein.xyz>
2026-04-02 01:20:19 +0530

    chore: add prettier plugins

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,5 +1,24 @@
 import { type Config } from "prettier";
 
-const config: Config = {};
+const config: Config = {
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindStylesheet: "src/styles/globals.css",
+  tailwindFunctions: ["cn", "twMerge", "tw", "clsx", "cva"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^react",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(?!assets)",
+    "^./",
+    "^@material-symbols",
+    ".css$",
+    "^@/assets",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
 
 export default config;
