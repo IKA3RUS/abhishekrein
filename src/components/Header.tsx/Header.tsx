4005d7fe864d7f8abf4221c6c00685480681b7cb
IKA3RUS <w@abhishekrein.xyz>
2026-04-26 16:02:55 +0530

    refactor: move header to layout

--- src/components/Header.tsx/Header.tsx
+++
@@ -1,7 +0,0 @@
-import Logo from "@/assets/common/logo.svg?react";
-
-function Header() {
-  return <Logo className="fixed m-1/2 top-5 fill-white z-10 w-full" />;
-}
-
-export { Header };
---
+++ src/components/layout/Header.tsx/Header.tsx
@@ -0,0 +1,7 @@
+import Logo from "@/assets/common/logos/ika3rus-logo.svg?react";
+
+function Header() {
+  return <Logo className="m-1/2 fixed top-5 z-10 w-full fill-white" />;
+}
+
+export { Header };
